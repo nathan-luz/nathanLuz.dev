02c97bbc1b14474d9e53a23785cb1737c7d0b2b8
Nathan Luz <nathanluz@Nathans-MacBook-Pro.local>
2024-07-31 22:07:02 -0300

    adding pages and ignoring IDE files

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+/**
+ * Represents the home page component. 
+ * Upper case 'H' in Home is important, 
+ * for Next.js to recognize it as a page component.
+ */
+function Home() {
+     return <div>Welcome to Next.js! YEAH!</div>
+}
+// Use the 'export' keyword to make the component available to other modules
+// Use default so Next.js knows this is the default component to be exported
+export default Home
